4b55d105b2212b94c5e0e59e6fb109e213be4528
pyyjac <111088015+pyyjac@users.noreply.github.com>
2023-01-03 22:24:32 +0100

    Update run.sh
    Signed-off-by: pyyjac <111088015+pyyjac@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ apk add --no-cache git
 yarn add --no-lockfile -D tailwindcss $INPUT_PLUGINS
 yarn run tailwindcss -f -i $INPUT_INPUT -o $INPUT_OUTPUT
 git config --global --add safe.directory /github/workspace
-git add $INPUT_OUTPUT
+git add $INPUT_OUTPUT -f
